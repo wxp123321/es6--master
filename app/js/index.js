be30e35ce5bbebbae926f4701135fb91e97d4014
XP <1648557071@qq.com>
2017-12-13 23:04:30 +0800

    17-12-13

--- app/js/index.js
+++ app/js/index.js
@@ -1,19 +1,2 @@
-//import 'babel-polyfill';
-//import './class/lesson14';
-
-//class Test{
-//    constructor(){
-//        this.a = 'hello world'
-//    }
-//
-//}
-//
-//let test = new Test();
-//
-//document.body.innerHTML = test.a;
-
-//import {A,test,Hello} from './class/lesson14';
-//import * as lesson from './class/lesson14';
-import Lesson17 from './class/lesson14';
-
-console.log(Lesson17.A);
+import 'babel-polyfill';
+import Lottery from 'lottery';
---
+++ app/js/lottery.js
@@ -0,0 +1,7 @@
+/**
+ * Created by XP on 2017/12/13.
+ */
+import './lottery/base';
+import './lottery/time';
+import './lottery/calculate';
+import './lottery/interface';
