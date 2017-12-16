0cbccaccce1db2d0c8669a2ed09cedf8bff8ba1e
XP <1648557071@qq.com>
2017-12-16 23:31:38 +0800

    17-12-16

--- app/js/lottery/base.js
+++ app/js/lottery/base.js
@@ -83,6 +83,119 @@ class Base{
         let $cur = $(e.currentTarget);
         let index = $cur.index();
         $('.boll-list .btn-boll').removeClass('btn-boll-active');
+        if(index == 0){
+            $(".boll-list .btn-boll").addClass('btn-boll-active');
+        }
+        if(index === 1){
+            $('.boll-list .btn-boll').each(function (i,t) {
+                if(t.textContent-5 > 0){
+                    $(t).addClass('btn-boll-active')
+                }
+            })
+        }
+        if(index === 2){
+            $('.boll-list .btn-boll').each(function (i,t) {
+                if(t.textContent-6 > 0){
+                    $(t).addClass('btn-boll-active')
+                }
+            })
+        }
 
+        if(index === 3){
+            $('.boll-list .btn-boll').each(function (i,t) {
+                if(t.textContent%2 == 1){
+                    $(t).addClass('btn-boll-active')
+                }
+            })
+        }
+
+        if(index === 4){
+            $('.boll-list .btn-boll').each(function (i,t) {
+                if(t.textContent%2 == 0){
+                    $(t).addClass('btn-boll-active')
+                }
+            })
+        }
+        self.getCount();
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    addCode(){
+        let self = this;
+        let $active = $('.boll-list .btn-boll-active').text().match(/\d{2}/g);
+        let active = $active?$active.length:0;
+        let count = self.computeCount(active,self,cur_play);
+        if(count){
+            self.addCodeItem($active.join(''),self.cur_play,self.play_list.get(self.cur_play).name,count);
+        }
+    }
+
+    addCodeItem(code,type,typeName,count){
+        let self = this;
+        //字符串模板 可用${}代替数据
+        const tpl = `
+            html代码
+        `;
+        $(self.cart_el).append(tpl);
+        self.getToal();
+    }
+
+    getCount(){
+        let self = this;
+        let active = $('.boll-list .btn-boll-active').length;
+        let count = self.computeCount(active,self,cur_play);
+        let range = self.computeBonus(active,self.cur_play);
+        let money = count*2;
+        let win1 = range[0] - money;
+        let win2 = range[1] - money;
+        let tpl;
+        let c1 = (win1<0&&win2<0)?Math.abs(win1):win1;
+        let c2 = (win1<0&&win2<0)?Math.abs(win2):win2;
+        if(count === 0){
+            tpl = `html`
+        }else if(range[0] === range[1]){
+            tpl = `html`
+        }else{
+            tpl = `html`
+        }
+        $('.sel_info').html(tpl);
+    }
+
+    getTotal(){
+        let count = 0;
+        $('.codelist li').each(function (index,item) {
+            count+=$(item).attr(count)*1;
+        })
+        $('#count').text(count);
+        $('#money').text(count*2);
+    }
+
+    getRandom(num){
+        let arr = [], index;
+        let number = Array.from(this.number);
+        while(num--){
+            index = Number.parseInt(Math.random()*number.length);
+            arr.push(number[index]);
+            number.splice(index,1);
+        }
+        return arr.join(' ')
+    }
+
+    getRandomCode(e){
+        e.preventDefault();
+        let num = e.currentTarget.getAttribute('count');
+        let self = this;
+        if(num === '0'){
+            $(self.cart_el).html('')
+        }else{
+            for(let i = 0;i < num;i++){
+                self.addCodeItem(self.getRandom(play),self.cur_play,self.play_list.get(self.cur_play).name,1);
+            }
+        }
     }
 }
+
+export default Base
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var mockjs = require('mockjs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+var makeIssue = function () {
+  var date = new Date();
+  var first_issue_data = new Date();
+  first_issue_data.setHours(9);
+  first_issue_data.setMinutes(10);
+  first_issue_data.setSeconds(0);
+  var end_issue_date = new Date(first_issue_data.getTime()+77*10*60*1000);
+
+
+  var cur_issue,end_time,state;
+  if(date.getTime() - first_issue_data.getTime() > 0&&date.getTime()-end_issue_date.getTime() < 0){
+
+  }
+}
 
 module.exports = router;
